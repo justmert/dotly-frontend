60b3f6c4a1a23a6c81bd11fb9290718cf00683c5
Mert Köklü <37740842+justmert@users.noreply.github.com>
2023-10-08 14:25:54 +0300

    identity component updated

--- src/components/overview/Identity.tsx
+++ src/components/overview/Identity.tsx
@@ -9,6 +9,9 @@ import CardHeader from "@/modules/Card/Header/Header";
 import CardLoader from "@/modules/Card/Loader/Loader";
 import NoData from "@/modules/Card/NoData/NoData";
 
+import { MdCheckCircle, MdCancel } from "react-icons/md";
+import { IIdentity } from "@/types/overview/types";
+
 export default function Identity() {
   const { identity, isLoading } = useIdentity();
   const router = useRouter();
@@ -31,6 +34,65 @@ export default function Identity() {
       <NoData element={<CardHeader title="Identity" tooltip="" />} message="" />
     );
 
+  function Badge({
+    name,
+    status,
+    link,
+    iconStart,
+  }: {
+    name: string;
+    status: any;
+    link?: string;
+    iconStart?: React.ReactElement;
+  }) {
+    return (
+      <div className="flex justify-between items-center gap-2 py-3 px-4 bg-gray-100 border border-indigo-300 rounded hover:bg-gray-200">
+        <div className="flex items-center gap-2">
+          {iconStart}
+          <span className="font-semibold">{name}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="font-semibold text-indigo-600">{status}</span>
+        </div>
+      </div>
+    );
+  }
+
+  function RewardListCardItem({ data }: { data: IIdentity; index: number }) {
+    return (
+      <div className="grid grid-cols-4 gap-6">
+        <div className="md:col-span-4 col-span-4">
+          <Badge
+            name="Network"
+            status={data.network}
+            iconStart={<MdCheckCircle className="text-green-900 text-lg" />}
+          />
+        </div>
+        <div className="md:col-span-4 col-span-4">
+          <Badge
+            name="Identity"
+            status={String(data.parent.identity)}
+            iconStart={<MdCheckCircle className="text-green-900 text-lg" />}
+          />
+        </div>
+        <div className="md:col-span-4 col-span-4">
+          <Badge
+            name="Display"
+            status={data?.parent?.display}
+            iconStart={<MdCheckCircle className="text-green-900 text-lg" />}
+          />
+        </div>
+        <div className="md:col-span-4 col-span-4">
+          <Badge
+            name="Sub Symbol"
+            status={data?.parent?.sub_symbol}
+            iconStart={<MdCheckCircle className="text-green-900 text-lg" />}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <Layout>
       <CardHeader
@@ -38,6 +100,9 @@ export default function Identity() {
         tooltip=""
         route={router.asPath + "#identity"}
       />{" "}
+      <div>
+        <RewardListCardItem data={identity[0]} index={0} />
+      </div>
     </Layout>
   );
 }
